0813f2016945c0b9542fbf7943ade36c8cc8705b
Alexander0204 <kraskovalexander@mail.ru>
2023-02-24 18:59:45 +0400

    lesson_3

---
+++ client.py
@@ -0,0 +1,34 @@
+import json
+import socket
+import sys
+
+from utils import create_presence, receive_message, process_server_answer, send_message, PORT, HOST
+
+
+def main():
+    try:
+        server_address = sys.argv[1]
+        server_port = int(sys.argv[2])
+        if server_port < 1024 or server_port > 65535:
+            raise ValueError
+    except IndexError:
+        server_address = HOST
+        server_port = PORT
+    except ValueError:
+        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        sys.exit(1)
+
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect((server_address, server_port))
+    message_to_server = create_presence()
+    send_message(clientsocket, message_to_server)
+
+    try:
+        answer = process_server_answer(receive_message(clientsocket))
+        print(answer)
+    except (ValueError, json.JSONDecodeError):
+        print('Не удалось декодировать сообщение сервера.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,53 @@
+import json
+import socket
+import sys
+
+from utils import receive_message, process_client_message, send_message, PORT, MAX_CONNECTIONS
+
+
+def main():
+    try:
+        if '-p' in sys.argv:
+            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            listen_port = PORT
+        if listen_port < 1024 or listen_port > 65535:
+            raise ValueError
+    except IndexError:
+        print('После параметра -\'p\' необходимо указать номер порта.')
+        sys.exit(1)
+    except ValueError:
+        print(
+            'В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        sys.exit(1)
+
+    try:
+        if '-a' in sys.argv:
+            listen_address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            listen_address = ''
+
+    except IndexError:
+        print(
+            'После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
+        sys.exit(1)
+
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((listen_address, listen_port))
+    serversocket.listen(MAX_CONNECTIONS)
+
+    while True:
+        client, address = serversocket.accept()
+        try:
+            msg_client = receive_message(client)
+            print(msg_client)
+            response = process_client_message(msg_client)
+            send_message(client, response)
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            print('Принято некорретное сообщение от клиента.')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
