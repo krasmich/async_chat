ce7c8e9f394e18eb699d6b984e0cc9bf20cf025f
krasmich <kraskovmichael@gmail.com>
2023-03-08 21:36:58 +0400

    lesson-7

--- utils.py
+++ utils.py
@@ -2,8 +2,9 @@ import json
 import logging
 import time
 from decor import log
+from errors import ReqFieldMissingError, ServerError
 
-PORT = 7778
+PORT = 7777
 HOST = '127.0.0.1'
 MAX_CONNECTIONS = 5
 MAX_PACKAGE_LENGTH = 1024
@@ -13,12 +14,13 @@ ACTION = 'action'
 TIME = 'time'
 USER = 'user'
 ACCOUNT_NAME = 'account_name'
+SENDER = 'sender'
 
 PRESENCE = 'presence'
 RESPONSE = 'response'
 ERROR = 'error'
-
-# LOGGING_LEVEL = logging.DEBUG
+MESSAGE = 'message'
+MESSAGE_TEXT = 'mess_text'
 
 
 server_logger = logging.getLogger('server')
@@ -60,24 +62,33 @@ def send_message(sock, message):
 
 
 @log
-def process_client_message(message):
+def process_client_message(message, messages_list, client):
     """
-    Обработчик сообщений от клиентов, принимает словарь -
-    сообщение от клиента, проверяет корректность,
-    возвращает словарь-ответ для клиента
-
+    Обработчик сообщений от клиентов, принимает словарь - сообщение от клиента,
+    проверяет корректность, отправляет словарь-ответ для клиента с результатом приёма.
     :param message:
+    :param messages_list:
+    :param client:
     :return:
     """
 
     server_logger.debug(f'Разбор сообщения от клиента : {message}')
     if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
             and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
-        return {RESPONSE: 200}
-    return {
-        RESPONSE: 400,
-        ERROR: 'Bad Request'
-    }
+        send_message(client, {RESPONSE: 200})
+        return
+        # Если это сообщение, то добавляем его в очередь сообщений. Ответ не требуется.
+    elif ACTION in message and message[ACTION] == MESSAGE and \
+            TIME in message and MESSAGE_TEXT in message:
+        messages_list.append((message[ACCOUNT_NAME], message[MESSAGE_TEXT]))
+        return
+        # Иначе отдаём Bad request
+    else:
+        send_message(client, {
+            RESPONSE: 400,
+            ERROR: 'Bad Request'
+        })
+        return
 
 
 @log
@@ -110,5 +121,6 @@ def process_server_answer(message):
     if RESPONSE in message:
         if message[RESPONSE] == 200:
             return '200 : OK'
-        return f'400 : {message[ERROR]}'
-    raise ValueError
+        elif message[RESPONSE] == 400:
+            raise ServerError(f'400 : {message[ERROR]}')
+    raise ReqFieldMissingError(RESPONSE)
